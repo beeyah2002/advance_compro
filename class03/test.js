e95242789502fc855ae3691bbe4ecbd6f9e44935
beeyah2002 <beesui2545@gmail.com>
2023-07-24 23:57:45 +0700

    beeyahhh

--- class03/test.js
+++ class03/test.js
@@ -1,23 +1,35 @@
 let values1 = ['Apple' , '1' , false] ;
 let values2 = ['Fries', 2 , true];
-let values3 = ['mars', 9 , 'Apple'];
+let values3 = ['mars', 2 , 'Apple'];
 
-let newval = values1.concat(values2,values3); // list `รวม
-//console.log(newval);
-var keep = []
-let result = newval.map((newval) => {
-    console.log(newval);
-    for ( i of newval){
-        if (i ==)
-    }
-    
-});
-for ( let count1 of newval ) {
-    keep = [count1];
-    
 
+
+
+for (let i = 0;  i < values1.length; i++){
+    for (let j = 0; j < values2.length; j++) {
+        if (values1[i] === values2[j]){
+            console.log(values1[i]);
+        }
     }
-    
-    
-    
-    
+}
+
+for (let i = 0;  i < values1.length; i++){
+    for (let j = 0; j < values3.length; j++) {
+        if (values1[i] === values3[j]){
+            console.log(values1[i]);
+        }
+    }
+}
+
+for (let i = 0;  i < values2.length; i++){
+    for (let j = 0; j < values3.length; j++) {
+        if (values2[i] === values3[j]){
+            console.log(values2[i]);
+        }
+    }
+}
+
+
+
+
+
