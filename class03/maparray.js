50cd8f75be3a857cd89b244c7f7ed0799b0a8083
beeyah2002 <s6506022420045@email.kmutnb.ac.th>
2023-08-22 12:02:11 +0700

    beeyah test

--- class03/maparray.js
+++ class03/maparray.js
@@ -3,4 +3,4 @@ const finalPaticipants = ["Taylor", "Donald" , "Don", "Natasha", "Bobby"];
 const announcements = finalPaticipants.map((member) => {
     return member + " joined the contest" ;
 });
-console.log(announcements);ทนืาำั
+console.log(announcements);
