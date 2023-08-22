5a4f45dbbed96590e498f81eb5269551741b55f2
beeyah2002 <s6506022420045@email.kmutnb.ac.th>
2023-08-22 12:03:03 +0700

    beeyahh

---
+++ class07/test17.js
@@ -0,0 +1,13 @@
+function longestvalidParentheses(string) {
+    for (let index = 0 ; index < string.length ; index ++) {
+        let count = 0;
+        if ( string[index] == "(" ) {
+            if ( string[index+1] == ")")
+            count + 1
+            
+
+        } 
+    }
+
+}
+longestvalidParentheses("(()")
